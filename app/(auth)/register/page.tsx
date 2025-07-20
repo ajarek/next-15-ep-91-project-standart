7d01cf38c3ba04410bafadc5e1ed0daf22195502
Ajarek <ajarek@poczta.onet.pl>
2025-07-21 00:06:26 +0200

    Improved error handling on the login page: changed searchParams type to Promise and updated error display

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -6,12 +6,13 @@ export const metadata: Metadata = {
   title: 'Register Page',
 }
 
-const RegisterPage = ({ searchParams }: { searchParams: { error?: string } }) => {
+const RegisterPage = async ({ searchParams }: { searchParams: Promise<{ error?: string }> }) => {
+  const error = await searchParams
   return <div className='min-h-[calc(100vh-128px)] flex flex-col items-center justify-center gap-4'>
     <h1 className='text-3xl font-semibold'>Register</h1>
-    {searchParams.error && (
+    {error && (
       <div className='text-red-500'>
-        {decodeURIComponent(searchParams.error)}
+        {decodeURIComponent(error .error || '')}
       </div>
     )}
     <RegisterForm/>
